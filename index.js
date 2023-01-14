a68a71dc7bdf92c4dce71f8d8348e295b11c249c
Lam Ngo <lam.ngo.kms@editshare.com>
2023-01-14 21:41:01 +0700

    Add authorization with passportjs lib

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
 const { logger, errorLogger } = require('./utils/logger')
+const { ensureAuthenticated } = require('./utils/common')
 require('dotenv').config()
 
 const authRoutes = require('./routes/auth')
 
 const app = express()
 
+const sess = {
+  secret: '42race xyz',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
+if (process.env.ENV === 'production') {
+  app.set('trust proxy', 1) 
+  sess.cookie.secure = true 
+}
+app.set('view engine', 'pug')
+app.use(session(sess))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger)
 /* static files */
 app.use(express.static('public'))
 app.use('/authorize', authRoutes)
+app.use(ensureAuthenticated)
 app.use(errorLogger)
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,58 @@
 const express = require('express')
+const passport = require('passport')
+const StravaStrategy = require('passport-strava-oauth2').Strategy
+const { ensureAuthenticated } = require('../utils/common')
+
+const STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID
+const STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET
+const STRAVA_CALLBACK_URL = process.env.STRAVA_CALLBACK_URL
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.redirect(`http://www.strava.com/oauth/authorize?client_id=${process.env.STRAVA_CLIENT_ID}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read`)
-})
+StravaStrategy.prototype.authorizationParams = function(options) {
+    return { approval_prompt: options.approvalPrompt }
+}
+
+
+passport.use(new StravaStrategy({
+    clientID: STRAVA_CLIENT_ID,
+    clientSecret: STRAVA_CLIENT_SECRET,
+    callbackURL: STRAVA_CALLBACK_URL,
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+router.get('/', function(req, res, next){
+    passport.authenticate('strava', { scope: ['read'], approvalPrompt: "force" })(req, res, next)
+  });
+
+
+router.get('/strava/callback', 
+  passport.authenticate('strava', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/authorize/auth-success');
+  });
+
+router.use(ensureAuthenticated)
+router.get('/auth-success', function(req, res, next){
+    const userID = req.user.id
+    res.render('auth-success', { athletUrl: `https://wwww.strava.com/athletes/${userID}`})
+});
+router.get('/disconnect', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router
